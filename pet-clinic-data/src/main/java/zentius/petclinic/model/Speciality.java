7cf07e67c10f7b349703ea2fbb1a5e6a3190c372
kyleisgorener <kyleisgorener@gmail.com>
2020-09-29 10:04:50 +0300

    added Speciality entity, closes #28

---
+++ pet-clinic-data/src/main/java/zentius/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package zentius.petclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
